3fed785c9bd205edf1698da427e4083214643725
Vitor Soares <vitorratollasoares@gmail.com>
2024-11-25 10:58:49 -0300

    First Commit - Project Init + Add Dependencies

---
+++ email-service/src/test/java/com/ratolla/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratolla.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
