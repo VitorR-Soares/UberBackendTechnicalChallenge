3e341a6d79030c372826cdc9945bf56cb8646c1c
Vitor Soares <vitorratollasoares@gmail.com>
2024-11-25 11:17:06 -0300

    Creation of Core, Application and Interface Adapter

---
+++ email-service/src/main/java/com/ratolla/email_service/adapters/EmailSenderGateway.java
@@ -0,0 +1,5 @@
+package com.ratolla.email_service.adapters;
+
+public interface EmailSenderGateway {
+    void sendEmail(String to, String subject, String body);
+}
---
+++ email-service/src/main/java/com/ratolla/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.ratolla.email_service.application;
+
+import com.ratolla.email_service.adapters.EmailSenderGateway;
+import com.ratolla.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGatweay;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGatweay = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGatweay.sendEmail(to, subject, body);
+    }
+}
